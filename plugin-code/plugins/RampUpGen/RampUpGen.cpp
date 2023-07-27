848048fa2b4ce86e7c324e0b35ada6a67f14af7f
Michael McCrea <mccrea.michael@gmail.com>
2023-07-27 21:25:52 +0300

    UGen: fix initialization in constructor
    + use a local variable in ramp generator function, not the member variable.
    + set read/write m_phase at head/tail of the block calc.
    + explicitly set current output value, then increment phase.
    + add comments.

--- plugin-code/plugins/RampUpGen/RampUpGen.cpp
+++ plugin-code/plugins/RampUpGen/RampUpGen.cpp
@@ -10,71 +10,98 @@ namespace RampUpGen {
 
 RampUpGen::RampUpGen() {
 
-  // Set the UGen's calculation function depending on the rate of the first
-  // argument (frequency)
-  if (inRate(Frequency) == calc_FullRate) {
-    mCalcFunc = make_calc_function<RampUpGen, &RampUpGen::next_a>();
+  // Initialize the state of member variables that depend on input aruguments
+  m_frequency = in0(Frequency);
 
-    // Calculate first value
+  // Set the UGen's calculation function depending on the rate of the first
+  // argument (frequency). 
+  // Call that function for one calculation cycle, which  generates an 
+  // initialization sample for downstream UGens
+  if (isAudioRateIn(Frequency)) {
+    mCalcFunc = make_calc_function<RampUpGen, &RampUpGen::next_a>();
     next_a(1);
   } else {
     mCalcFunc = make_calc_function<RampUpGen, &RampUpGen::next_k>();
-
-    // Calculate first value
     next_k(1);
   };
+
+  // Reset the initial state of member variables.
+  m_phase = 0.0;
+  // This is so the initialization sample calulated above by 'next' matches
+  // the first output sample when the synth is run and 'next' is called again.
+  // m_frequency is not reset because it's initial value is unaffected by 
+  // the next_k(1)
 }
 
-// The guts of our ramp generator
-inline float RampUpGen::progressPhasor(float frequency) {
+// The ramp generator
+inline float RampUpGen::progressPhasor(double phase, float frequency) {
   // Calculate increment value.
   // Double precision is important in phase values
   // because division errors are accumulated as well
   double increment = static_cast<double>(frequency) / sampleRate();
-
-  m_phase += increment;
-
+  
+  phase += increment;
+  
   const double minvalue = 0.0;
   const double maxvalue = 1.0;
 
-  // Wrap the phasor if it goes beyond the boundaries
-  if (m_phase > maxvalue) {
-    m_phase = minvalue + (m_phase - maxvalue);
-  } else if (m_phase < minvalue) {
-    // in case phase is below minimum value
-    m_phase = maxvalue - std::fabs(m_phase);
+  // Wrap the phasor if it goes above maxvalue or below minvalue
+  if (phase > maxvalue) {
+    phase = minvalue + (phase - maxvalue);
+  } else if (phase < minvalue) {
+    phase = maxvalue - std::fabs(phase);
   }
 
-  return m_phase;
+  return phase;
 }
 
 // Calculation function for audio rate frequency input
 void RampUpGen::next_a(int nSamples) {
   const float *frequency = in(Frequency);
   float *outbuf = out(0);
-
+  double current_phase = m_phase;
+  
   for (int i = 0; i < nSamples; ++i) {
-    outbuf[i] = progressPhasor(frequency[i]);
+    // Be sure to read from UGen's inputs BEFORE writing to outputs, 
+    // they share a buffer by default!
+    const float freq = frequency[i];
+    
+    // Write out the phase
+    outbuf[i] = current_phase;
+
+    // Advance the phase
+    current_phase = progressPhasor(current_phase, freq);
   }
+
+  // Store final value of phase to be used next time the
+  // calculation function runs
+  m_phase = current_phase;
 }
 
 // Calculation function for control rate frequency input
 void RampUpGen::next_k(int nSamples) {
   const float frequencyParam = in(Frequency)[0];
   SlopeSignal<float> slopedFrequency =
-      makeSlope(frequencyParam, m_frequency_past);
+    makeSlope(frequencyParam, m_frequency);
   float *outbuf = out(0);
+  double current_phase = m_phase;
 
   for (int i = 0; i < nSamples; ++i) {
     const float freq = slopedFrequency.consume();
 
-    outbuf[i] = progressPhasor(freq);
+    // Write out the phase
+    outbuf[i] = current_phase;
+
+    // Advance the phase
+    current_phase = progressPhasor(current_phase, freq);
   }
 
-  // Store final value of frequency slope to be used next time the calculation
-  // function runs
-  m_frequency_past = slopedFrequency.value;
+  // Store final value of frequency and phase to be used next time the
+  // calculation function is called
+  m_frequency = slopedFrequency.value;
+  m_phase = current_phase;
 }
+
 } // namespace RampUpGen
 
 PluginLoad(RampUpGenUGens) {
